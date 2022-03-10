4df2c835906a5f7c9f475891f3e5afe3e6cc943b
ZhenyaGo <86401932+ZhenyaGo@users.noreply.github.com>
2022-03-10 21:43:37 +0300

    add_request_param_annotation

--- src/main/java/com/github/zhenya/springmvccource/controllers/FirstController.java
+++ src/main/java/com/github/zhenya/springmvccource/controllers/FirstController.java
@@ -3,13 +3,15 @@ package com.github.zhenya.springmvccource.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(required = false) String name, @RequestParam(required = false) String surname) {
+        System.out.println("Hello! " + name + " " + surname);
      return "first/hello";
     }
 
